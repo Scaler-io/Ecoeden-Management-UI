bfd0b3e2e78f881ff61ab15491bca4af62326180
sharthak1095 <sharthakmallik@gmail.com>
2024-08-21 00:10:51 +0530

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ecoeden-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'Ecoeden-Management-UI';
+}
