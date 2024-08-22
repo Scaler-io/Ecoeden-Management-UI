4a20c514bad52844273553c8d0862310766a09e5
sharthak1095 <sharthakmallik@gmail.com>
2024-08-22 22:12:26 +0530

    basic app setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'ecoeden-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'Ecoeden-Management-UI';
