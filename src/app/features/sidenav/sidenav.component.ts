e3f349140dcd7896e10bbccff393550620d89e94
sharthak1095 <sharthakmallik@gmail.com>
2024-09-30 01:56:53 +0530

    adds sidenav submenu feature

--- src/app/features/sidenav/sidenav.component.ts
+++ src/app/features/sidenav/sidenav.component.ts
@@ -1,43 +1,67 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { select, Store } from '@ngrx/store';
-import { getMobileViewState } from 'src/app/state/mobile-view/mobile-view.selector';
-import { getSidenavToggleState } from 'src/app/state/sidenav/sidenav.selector';
-import { AppState } from 'src/app/store/app.state';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {select, Store} from '@ngrx/store';
+import {getMobileViewState} from 'src/app/state/mobile-view/mobile-view.selector';
+import {getSidenavToggleState} from 'src/app/state/sidenav/sidenav.selector';
+import {AppState} from 'src/app/store/app.state';
 
 import * as sidenavToggleAction from '../../state/sidenav/sidenav.action';
 
 @Component({
   selector: 'ecoeden-sidenav',
   templateUrl: './sidenav.component.html',
-  styleUrls: ['./sidenav.component.scss'],
+  styleUrls: ['./sidenav.component.scss']
 })
 export class SidenavComponent implements OnInit, OnDestroy {
   public sidenavExpanded: boolean;
   public isMobileView: boolean;
+  public subMenuList = {
+    users: false,
+    products: false
+  };
 
   constructor(private store: Store<AppState>) {}
 
   private subscriptions = {
     sidenavToggleState: null,
-    mobileViewState: null,
+    mobileViewState: null
   };
 
   ngOnInit(): void {
-    this.subscriptions.sidenavToggleState = this.store
-      .pipe(select(getSidenavToggleState))
-      .subscribe((state) => {
-        this.sidenavExpanded = state;
-      });
-    this.subscriptions.mobileViewState = this.store
-      .pipe(select(getMobileViewState))
-      .subscribe((state) => {
-        this.isMobileView = state;
-      });
+    this.subscriptions.sidenavToggleState = this.store.pipe(select(getSidenavToggleState)).subscribe(state => {
+      this.sidenavExpanded = state;
+      if (!state) {
+        this.subMenuList = {
+          ...this.subMenuList,
+          users: false,
+          products: false
+        };
+      }
+    });
+    this.subscriptions.mobileViewState = this.store.pipe(select(getMobileViewState)).subscribe(state => {
+      this.isMobileView = state;
+    });
   }
 
-  public handleSidenavItemClick() {
-    !this.sidenavExpanded &&
-      this.store.dispatch(new sidenavToggleAction.ToggleSideNav());
+  public handleSidenavItemClick(menu?: string) {
+    if (menu && menu === 'users') {
+      this.subMenuList = {
+        ...this.subMenuList,
+        users: !this.subMenuList.users,
+        products: false
+      };
+    }
+    if (menu && menu === 'products') {
+      this.subMenuList = {
+        ...this.subMenuList,
+        users: false,
+        products: !this.subMenuList.products
+      };
+    }
+    !this.isMobileView && !this.sidenavExpanded && this.store.dispatch(new sidenavToggleAction.ToggleSideNav());
+  }
+
+  public hadndleSubmenuClick() {
+    this.isMobileView && !this.sidenavExpanded && this.store.dispatch(new sidenavToggleAction.ToggleSideNav());
   }
 
   ngOnDestroy(): void {
